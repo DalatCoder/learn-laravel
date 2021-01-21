a331953bf04355307241baa1ab16ed544be56501
TrongHieu <hieunctk42@gmail.com>
2021-01-21 08:17:29 +0700

    Styling using bootstrap

--- todos-app/resources/views/todos/index.blade.php
+++ todos-app/resources/views/todos/index.blade.php
@@ -5,12 +5,30 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Todos</title>
 </head>
 <body>
-<ul>
-    @foreach($todos as $todo)
-        <li>{{ $todo->name }}</li>
-    @endforeach
-</ul></body>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card my-5">
+                <div class="card-header text-center">
+                    Todos
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        @foreach($todos as $todo)
+                            <li class="list-group-item">
+                                {{ $todo->name }}
+                                <button class="btn btn-primary btn-sm float-right">View</button>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
 </html>
