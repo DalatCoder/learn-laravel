45d60116a1884cc45ea102cc958ed782b7992d4a
DalatCoder <hieuntctk42@gmail.com>
2021-01-22 18:01:32 +0700

    Add PostsController

--- cms-blog/routes/web.php
+++ cms-blog/routes/web.php
@@ -23,3 +23,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('categories', \App\Http\Controllers\CategoriesController::class);
+
+Route::resource('posts', \App\Http\Controllers\PostsController::class);
