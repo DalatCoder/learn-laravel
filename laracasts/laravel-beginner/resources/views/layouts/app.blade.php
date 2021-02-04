eae86c19dfaea117fdd8710b43c607d21b6eb12b
DalatCoder <hieuntctk42@gmail.com>
2021-02-04 12:11:03 +0700

    Laravel mix and webpack

--- laracasts/laravel-beginner/resources/views/layouts/app.blade.php
+++ laracasts/laravel-beginner/resources/views/layouts/app.blade.php
@@ -8,20 +8,30 @@
     <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet"/>
     <link href="css/default.css" rel="stylesheet" type="text/css" media="all"/>
     <link href="css/fonts.css" rel="stylesheet" type="text/css" media="all"/>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 <div id="header-wrapper">
     <div id="header" class="container">
         <div id="logo">
-            <h1><a href="#">SimpleWork</a></h1>
+            <h1><a href="/">SimpleWork</a></h1>
         </div>
         <div id="menu">
             <ul>
-                <li class="current_page_item"><a href="/" accesskey="1" title="">Homepage</a></li>
-                <li><a href="#" accesskey="2" title="">Our Clients</a></li>
-                <li><a href="/about" accesskey="3" title="">About Us</a></li>
-                <li><a href="#" accesskey="4" title="">Careers</a></li>
-                <li><a href="#" accesskey="5" title="">Contact Us</a></li>
+                <li class="{{ Request::path() === '/' ? 'current_page_item' : '' }}"><a href="/" accesskey="1" title="">Homepage</a>
+                </li>
+                <li class="{{ Request::path() === 'our-client' ? 'current_page_item' : '' }}"><a href="#" accesskey="2"
+                                                                                                 title="">Our
+                        Clients</a></li>
+                -
+                <li class="{{ Request::path() === 'about' ? 'current_page_item' : '' }}"><a href="/about" accesskey="3"
+                                                                                            title="">About Us</a></li>
+                <li class="{{ Request::path() === 'careers' ? 'current_page_item' : '' }}"><a href="#" accesskey="4"
+                                                                                              title="">Careers</a></li>
+                -
+                <li class="{{ Request::path() === 'contact' ? 'current_page_item' : '' }}"><a href="#" accesskey="5"
+                                                                                              title="">Contact Us</a>
+                </li>
             </ul>
         </div>
     </div>
@@ -29,6 +39,13 @@
 </div>
 
 @yield('content')
+
+<div id="copyright" class="container">
+    <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a
+            href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
+
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
 
