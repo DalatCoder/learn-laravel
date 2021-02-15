40dadabc41e8a3d81b4c0e1cdfb972f4da4ca4fa
DalatCoder <hieuntctk42@gmail.com>
2021-02-15 16:18:18 +0700

    Phần 31.1: Laravel Transaction - Đảm bảo toàn vẹn dữ liệu

--- LTV-fullstack/web-ban-hang/src/app/Http/Controllers/AdminProductController.php
+++ LTV-fullstack/web-ban-hang/src/app/Http/Controllers/AdminProductController.php
@@ -10,6 +10,8 @@ use App\ProductTag;
 use App\Tag;
 use App\Traits\StorageImageTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AdminProductController extends Controller
 {
@@ -46,47 +48,54 @@ class AdminProductController extends Controller
 
     public function store(Request $request)
     {
-        $dataProductCreate = [
-            'name' => $request['name'],
-            'price' => $request['price'],
-            'content' => $request['description'],
-            'user_id' => auth()->id(),
-            'category_id' => $request['category_id']
-        ];
+        try {
+            DB::beginTransaction();
 
-        $featureImage = $this->storageTraitUpload($request, 'feature_image_path', 'products');
-        if (!empty($featureImage)) {
-            $dataProductCreate['feature_image_name'] = $featureImage['file_name'];
-            $dataProductCreate['feature_image_path'] = $featureImage['file_path'];
-        }
-
-        $product = $this->product->create($dataProductCreate);
-
-        // Save list of detail images
-        if ($request->has('image_path')) {
-            foreach ($request['image_path'] as $item) {
-                $dataProductImageDetail = $this->storageTraitUploadMultiple($item, 'products');
-                $product->images()->create([
-                    'image_path' => $dataProductImageDetail['file_path'],
-                    'image_name' => $dataProductImageDetail['file_name']
-                ]);
+            $dataProductCreate = [
+                'name' => $request['name'],
+                'price' => $request['price'],
+                'content' => $request['description'],
+                'user_id' => auth()->id(),
+                'category_id' => $request['category_id']
+            ];
+            $featureImage = $this->storageTraitUpload($request, 'feature_image_path', 'products');
+            if (!empty($featureImage)) {
+                $dataProductCreate['feature_image_name'] = $featureImage['file_name'];
+                $dataProductCreate['feature_image_path'] = $featureImage['file_path'];
             }
-        }
+            $product = $this->product->create($dataProductCreate);// Save list of detail images
+            if ($request->has('image_path')) {
+                foreach ($request['image_path'] as $item) {
+                    $dataProductImageDetail = $this->storageTraitUploadMultiple($item, 'products');
+                    $product->images()->create([
+                        'image_path' => $dataProductImageDetail['file_path'],
+                        'image_name' => $dataProductImageDetail['file_name']
+                    ]);
+                }
+            }// Save list of tags
+            if ($request->has('tags')) {
+                $tagIds = [];
+                foreach ($request['tags'] as $item) {
+                    // Save to tags table
+                    $tag = $this->tag->firstOrCreate([
+                        'name' => $item
+                    ]);
 
-        // Save list of tags
-        if ($request->has('tags')) {
-            $tagIds = [];
-            foreach ($request['tags'] as $item) {
-                // Save to tags table
-                $tag = $this->tag->firstOrCreate([
-                   'name' => $item
-                ]);
+                    array_push($tagIds, $tag->id);
+                }
 
-                array_push($tagIds, $tag->id);
+                // Save to product_tags table
+                $product->tags()->attach($tagIds);
             }
 
-            // Save to product_tags table
-            $product->tags()->attach($tagIds);
+            DB::commit();
+            
+            return redirect()->route('products.index');
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            $message = 'Message: ' . $e->getMessage() . '. Line: ' . $e->getLine();
+            Log::error($message);
         }
     }
 
